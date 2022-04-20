b3163a29fee3eeef6d55a084211c26b5e000f2d8
Julia <batyulka@gmail.com>
2022-04-20 22:33:29 +0300

    The second task was completed

---
+++ CoursesKotlin/app/src/main/java/Lecture1_1/Main.kt
@@ -0,0 +1,22 @@
+package Lecture1_1
+
+import java.util.*
+
+fun main () {
+
+    val answers = listOf<String>("Yes", "No", "Likely yes", "Likely no", "Here is some potential",
+        "Incorrect form of the question")
+
+    do {
+        println("Ask a question")
+        val scan = Scanner(System.`in`)
+        val n = scan.nextLine()
+        if (n == "") {
+            break
+        }
+        val randomAnswer = (0..5).random()
+        println(answers[randomAnswer])
+    }
+    while(n != "")
+
+}
