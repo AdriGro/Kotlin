96ccc26ab32e2955b257c2c93109df22d66beff5
Julia <batyulka@gmail.com>
2022-04-23 22:09:05 +0300

    First homework done

---
+++ src/Lecture1/Main.kt
@@ -0,0 +1,28 @@
+package Lecture1
+
+
+private var userList : MutableList<Person> = mutableListOf()
+
+fun main (){
+
+    val user1 = Person("Juls", "Mihnyuk", 22)
+    val user2 = Person("Sashka", "Zhizhensky", 26)
+    add(user1)
+    add(user2)
+    showList()
+}
+
+
+private fun add (user: Person) {
+    userList.add(user)
+}
+
+fun toString(person: Person): String {
+    return "Name: ${person.name}  Surname:  ${person.secondName}  Age:  ${person.age}"
+}
+
+private fun showList () {
+    for (person in userList) {
+        println(toString(person))
+    }
+}
